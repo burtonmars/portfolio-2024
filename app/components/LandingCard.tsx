35b546c56d840a06094fed6a8a6a210e2cf22866
UBC Student <burtonmars91@gmail.com>
2024-07-27 22:10:30 -0700

    change landing card text

--- app/components/LandingCard.tsx
+++ app/components/LandingCard.tsx
@@ -9,7 +9,7 @@ const LandingCard = () => {
             <h2 className='text-center text-xl md:text-2xl xxl:text-3xl'>Marshal Burton</h2>
             <h1 className='text-center text-2xl md:text-3xl xxl:text-4xl font-bold'>Full Stack Developer</h1>
             <h3 className='text-center text-xl xxl:text-2xl'>I am a passionate developer and life-long learner who strives to combine the best software design practices with innovative and creative solutions.</h3>
-            <Link className='btn btn-primary btn-lg mt-6 text-black font-bold' href={'#projects'}>view my creations</Link>  
+            <Link className='btn btn-primary btn-lg mt-6 text-black font-bold' href={'#projects'}>view my projects</Link>  
         </div>
     </div>
   )
